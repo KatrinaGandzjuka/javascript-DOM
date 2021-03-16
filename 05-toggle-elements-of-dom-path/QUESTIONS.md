bf82c7f741b7657a9e92ec39df3aa5161d2c8a2a
KatrinaGandzjuka <katrina.gandzjuk@icloud.com>
2021-03-16 15:35:55 +0200

    05-toggle-elements-of-dom-path

--- 05-toggle-elements-of-dom-path/QUESTIONS.md
+++ 05-toggle-elements-of-dom-path/QUESTIONS.md
@@ -7,3 +7,4 @@
 > Describe the contract you used for finding the movies to toggle in the DOM. How is this function different from other functions that find elements in the DOM?
 
 Your reply here...
+I used querySelectorAll, because moovies is a list an we use query selector for lists
--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,14 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    const button = document.getElementById("toggle_button");
+    const leable = document.querySelectorAll('.second_five li');
+    button.addEventListener('click', function(){
+        leable.forEach((elements) => {
+            if(elements.style.display == 'list-item') {
+                elements.style.display = 'none'
+            } else {
+                elements.style.display = 'list-item'
+            }
+        });
+    });
+});
