b006bca027bca385668a559c51db58e7a14bacff
KatrinaGandzjuka <katrina.gandzjuk@icloud.com>
2021-03-16 15:26:32 +0200

    04-toggle-many-elements

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,14 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const toggle = document.getElementsByClassName("toggle_me");
+    const button = document.getElementById("button");
+    button.addEventListener('click', function(){
+        Array.from(toggle).forEach((elements) => {
+            if(elements.style.display == 'flex') {
+                elements.style.display = 'none'
+            }else{
+                elements.style.display = 'flex'
+            }
+        });
+    });
+});
