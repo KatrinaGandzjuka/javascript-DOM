cb93ebaaaab3d3e087419ed46fba64816100d4c5
KatrinaGandzjuka <katrina.gandzjuk@icloud.com>
2021-03-16 15:46:55 +0200

    06-detecting-typed-input

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,8 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function(){
+    const field = document.getElementById("full_name");
+  field.addEventListener("keyup", function() {
+    const div = document.getElementById("greeting");
+    div.innerHTML = ("Hello there, " + field.value + "!");
+  });
+});
