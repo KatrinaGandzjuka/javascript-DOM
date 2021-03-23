453d480e3d67dfaf9092c20c67bb710d2d1f4db2
KatrinaGandzjuka <katrina.gandzjuk@icloud.com>
2021-03-23 13:47:55 +0200

    Kludu labojums

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const moreLink = document.getElementById('more_text_link');
-  const moreText = document.getElementById('more_text_content');
+  const Link = document.getElementById('more_text_link');
+  const Text = document.getElementById('more_text_content');
 
-  moreLink.addEventListener('click', function(){
-    moreText.style.display = 'initial';
-    moreLink.style.display = 'none';
+  Link.addEventListener('click', function(){
+    Text.style.display = 'initial';
+    Link.style.display = 'none';
   });
 });
 
